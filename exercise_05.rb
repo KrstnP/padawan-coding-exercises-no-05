ab6c0387bcc5b99de423df2556f0eff20b119fd0
Tin <krstnpangan@gmail.com>
2018-07-28 20:15:02 +0800

    Convert km to miles

--- exercise_05.rb
+++ exercise_05.rb
@@ -1,9 +1,7 @@
 def greet(name)
   greeting = ["Hello", "Hi", "Ohai", "ZOMG"]
   greeting = greeting.shuffle
-  "#{greeting[0]} #{name}"
-end
-puts greet("Nicole")
+  "#{greeting[0]} #{name}"puts greet("Nicole")
 puts greet("Toby")
 puts greet("Kristine")
 puts greet("Wilson")
---
+++ exercise_06.rb
@@ -0,0 +1,9 @@
+def miles_to_kilometers(miles)
+  return miles * 1.60934
+end
+result = miles_to_kilometers(25)
+puts result
+
+
+
+# This should print out 40.2336
