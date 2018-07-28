469f71a29291ad6349b14dd9c9afc24719a24c74
Tin <krstnpangan@gmail.com>
2018-07-28 20:29:15 +0800

    Convert km to miles

--- exercise_07.rb
+++ exercise_07.rb
@@ -1,16 +1,10 @@
-puts 'Input a starting year:'
-start_year = gets.chomp
-puts 'Input an ending year:'
-end_year = gets.chomp
-puts ''
-
-while start_year.to_i <= end_year.to_i
-
-    if start_year.to_f%400 == 0
-        puts start_year
-    elsif start_year.to_f%100 == 0
-    elsif start_year.to_f%4 == 0
-        puts start_year
-    end
-    start_year = start_year.to_i + 1
+def leap_year?(year)
+  if year%4 = 0
+    return "yes"
+  else
+    return "no"
+  end
 end
+
+puts leap_year?(2012)
+puts leap_year?(2015)
